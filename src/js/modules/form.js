a196a1854959cee8eb1071c3a2897c92ac4c045f
Den Marshall <dank03052005@gmail.com>
2023-01-27 22:07:28 +0200

    Added preloader

--- src/js/modules/form.js
+++ src/js/modules/form.js
@@ -2,11 +2,15 @@ export default () => {
     'use strict'
     const goFutherBtn = document.querySelector('.choose__right-btn');
 
+    const chooseBtns = document.querySelector('.choose__buttons');
     const chooseBlock = document.querySelector('.choose__block');
     const chooseBlockFinal = document.querySelector('.choose__block-final');
-    const chooseBtns = document.querySelector('.choose__buttons');
+    const chooseBlockFinalImage = chooseBlockFinal.querySelector('source');
+    const chooseSliders = document.querySelectorAll('.choose__slider');
 
     goFutherBtn.addEventListener('click', () => {
+        chooseBlockFinalImage.srcset = Array.from(chooseSliders).find(slider => slider.style.display === 'flex').querySelector('.tns-slide-active').src
+        chooseBlockFinalImage.src = Array.from(chooseSliders).find(slider => slider.style.display === 'flex').querySelector('.tns-slide-active').src
         chooseBlock.classList.add('fadeOut_02s')
         chooseBtns.classList.add('fadeOut_02s')
         chooseBlock.addEventListener('animationend', () => {
@@ -18,11 +22,18 @@ export default () => {
         })
     })
 
-    const formInputsNotArray = document.querySelectorAll('.form__input-text')
+    // const formInputs = Array.from(document.querySelectorAll('.form__input-text'))
 
-    const formInputs = Array.from(formInputsNotArray)
+    // const checkForEmptyInput = () => formInputs.filter(input => input.value !== '').forEach(input => input.style.background = 'rgba(255, 255, 255, 0.08)')
 
-    const checkForEmptyInput = () => formInputs.filter(input => input.value !== '').forEach(input => input.style.background = 'rgba(255, 255, 255, 0.08)')
+    // formInputs.forEach(input => input.addEventListener('input', checkForEmptyInput))
 
-    formInputs.forEach(input => input.addEventListener('change', checkForEmptyInput))
+    const inputs = Array.from(document.querySelectorAll('form__input'))
+
+    const checkInput = () => {
+
+        inputs.filter(input => input.value.contains('@')) ? null : console.log('pizdec');
+    }
+
+    inputs.forEach(input => input.addEventListener('input', checkInput))
 }
